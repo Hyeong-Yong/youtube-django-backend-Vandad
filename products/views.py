4afa6cbc17cee7495a208a4eaa3d3d085f63a4de
Hyeongyong Hwang <hwanghyeongyong@gmail.com>
2023-02-26 12:20:28 +0900

    step-4

---
+++ products/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from products.models import ProductCategory, Maker, Product
+
+from products.serializers import (
+    MakerSerializer,
+    ProductCategorySerializer,
+    ProductSerializer,
+)
+
+
+class ProductCategoryListView(ListAPIView):
+    serializer_class = ProductCategorySerializer
+    queryset = ProductCategory.objects.all()
+
+
+class MakerListView(ListAPIView):
+    serializer_class = MakerSerializer
+    queryset = Maker.objects.all()
+
+
+class ProductsListView(ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+# [
+#     {
+
+#     }
+# ]
